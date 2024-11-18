0838694e71e82ea84222335bf546059b3a8adc53
MJSDaw <mjsdaw@gmail.com>
2024-11-18 16:35:59 +0000

    Create index.php

---
+++ frontend/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <header>
+        <h1>Inicio</h1>
+        <div id="navBar">
+            <a href="">Restaurantes</a>
+            <a href="">Pedidos</a>
+            <a href="">Productos</a>
+        </div>
+    </header>
+    <p>Bienvenido a la aplicación de gestion de tu almacen. Desde el menu de arriba podrás acceder a los diferentes menús que te ofrece.</p>
+</body>
+</html>
